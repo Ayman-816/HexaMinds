7981ea8fd51001184020bab585df285c7a3f9fc0
Ayman <streamingdude1999@gmail.com>
2026-03-18 01:13:38 +0200

    Pin definitions

--- Code/Code.ino
+++ Code/Code.ino
@@ -1,3 +1,29 @@
+#define AIN1 27
+#define AIN2 26
+#define PWMA 25  
+
+#define BIN1 33
+#define BIN2 32
+#define PWMB 14   
+
+#define STBY 13
+
+#define TRIG_FRONT 4
+#define ECHO_FRONT 16
+
+#define TRIG_LEFT 17
+#define ECHO_LEFT 5
+
+#define TRIG_RIGHT 18
+#define ECHO_RIGHT 19
+
+#define MPU_SDA 21
+#define MPU_SCL 22
+
+#define SERVO1 23
+#define SERVO2 12
+#define SERVO3 15
+
 void setup() {
   // put your setup code here, to run once:
 
