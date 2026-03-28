6f15aa3883d864623c390cb08e7e8a68a6597a0f
Ayman Mohamed <streamingdude1999@gmail.com>
2026-03-29 00:43:21 +0200

    Update Config.h

--- Main/Config.h
+++ Main/Config.h
@@ -1,1 +1,35 @@
 #pragma once
+
+// ================= I2C (PCA9685 + MPU6050) =================
+#define I2C_SDA 21
+#define I2C_SCL 22
+
+// ================= ULTRASONIC SENSORS =================
+
+// Front Sensor
+#define US_FRONT_TRIG 32
+#define US_FRONT_ECHO 33 
+
+// Left Sensor
+#define US_LEFT_TRIG 25
+#define US_LEFT_ECHO 26  
+
+// Right Sensor
+#define US_RIGHT_TRIG 27
+#define US_RIGHT_ECHO 14 
+
+// ================= MOTOR DRIVER (TB6612) =================
+
+// Left Motors
+#define MOTOR_L_PWM   18
+#define MOTOR_L_IN1   19
+#define MOTOR_L_IN2   23
+
+// Right Motors
+#define MOTOR_R_PWM   5
+#define MOTOR_R_IN1   17
+#define MOTOR_R_IN2   16
+
+// ================= OPTIONAL / FUTURE =================
+#define FREE_1 4
+#define FREE_2 13
